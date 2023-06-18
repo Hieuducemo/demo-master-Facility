ce83c4dc600b39be88c11876b8ddaaf1057c43bb
MinhQuanTo <115225096+MinhQuanTo@users.noreply.github.com>
2023-06-18 22:49:15 +0200

    Update authorizedFetch.js

--- src/queries/authorizedFetch.js
+++ src/queries/authorizedFetch.js
@@ -17,6 +17,6 @@ export const authorizedFetch = (path, params) => {
     const newParams = {...globalFetchParams, ...params} // allow owerwrite default parameters (globalFetchParams)
     const overridenPath = '/api/gql'
     return (
-        fetch(overridenPath, newParams) //params.header should be extended with Authorization TOKEN
+        fetch(overridenPath, newParams) 
     )
 }
