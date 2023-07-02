e445e48cab2d7ad0919beb97a5b6be32d03fee95
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2023-07-02 13:49:10 +0200

    small change

--- src/queries/authorizedFetch.js
+++ src/queries/authorizedFetch.js
@@ -9,14 +9,14 @@ const globalFetchParams = {
 
 /**
  * Wrapper function for the fetch API, creating a communication layer with the server.
- *
+ * * @param {*} path 
  * @param {Object} params - Additional parameters to customize the fetch request.
  * @returns {Promise<Response>} A promise representing the response to the fetch request.
  */
-export const authorizedFetch = (params) => {
+export const authorizedFetch = (path, params) => {
     const newParams = {...globalFetchParams, ...params} // allow owerwrite default parameters (globalFetchParams)
     const overridenPath = '/api/gql'
     return (
-        fetch(overridenPath, newParams) 
+        fetch(overridenPath, newParams) //params.header should be extended with Authorization TOKEN
     )
 }
